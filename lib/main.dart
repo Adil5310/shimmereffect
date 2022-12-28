4f30822613b4e49406d24c60e6124fed38a3df57
Adil Akbar Ali <adilakbarali1@gmail.com>
2022-12-28 13:33:19 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+import 'Shimmer_Effect.dart';
+import 'carousel_slider.dart';
+
+void main() => runApp(MyApp());
+
